dd21e1cffb568333363dc7ea746db8ab087fbba2
Erick Augusto <erickaugs@gmail.com>
2024-04-25 23:41:28 -0300

    Aplicação de PageObjects e reuso de browser

--- src/main/java/suites/SuiteTest.java
+++ src/main/java/suites/SuiteTest.java
@@ -1,11 +1,23 @@
 package suites;
 
 import core.DriverFactory;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.io.FileHandler;
 import test.TesteCadastro;
 import test.TesteCampoTreinamento;
+
+import java.io.File;
+import java.io.IOException;
+
+import static core.DriverFactory.getDriver;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -13,8 +25,14 @@ import test.TesteCampoTreinamento;
         TesteCampoTreinamento.class})
 public class SuiteTest {
 
-    @AfterClass
-    public static void finalizaTudo(){
+    @Rule
+    public TestName testName = new TestName();
+    @After
+    public void finalizaTudo() throws IOException {
+        TakesScreenshot ss = (TakesScreenshot) getDriver();
+        File file = ss.getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(file, new File(testName.getMethodName()+".jpg"));
+
         DriverFactory.killDriver();
     }
 }
